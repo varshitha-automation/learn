e001b75b74d48e7ce828a4be9db72abb82c8c345
varshitha-automation <varshitha.m29@gmail.com>
2021-05-17 14:37:07 +0530

    javaLearn

---
+++ src/firstjava/firstScreenshot.java
@@ -0,0 +1,15 @@
+package firstjava;
+
+import java.io.File;
+import java.io.IOException;
+
+
+
+public class firstScreenshot {
+	
+	public static void main(String[] args) {
+		 
+		System.out.println("hello");
+	}
+}
+	
